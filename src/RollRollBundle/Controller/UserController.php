47d9f664664bd92b72167ce681d8e46fde1789fb
Thibault Introvigne <introvig1u@fst-ats-311-l01.fst.site.univ-lorraine.fr>
2015-11-20 15:29:09 +0100

    Ajout login msg erreur

--- src/RollRollBundle/Controller/UserController.php
+++ src/RollRollBundle/Controller/UserController.php
@@ -22,8 +22,33 @@ class UserController extends Controller
             'method' => 'POST',
         ));
 
+         if ($form->handleRequest($request)->isValid()){
+
+         	$repo = $this->getDoctrine()->getRepository("RollRollBundle:Player");
+         	$user = $repo->findOneByPseudo($player->getPseudo());
+         	if($user){
+         		 return $this->render('RollRollBundle:Default:error.html.twig',array( //Login réussi
+        		'titre'=> "Login OK",
+        		'message'=> "Ok"
+        	));
+
+
+
+         	}else{
+         	return $this->render('RollRollBundle:Default:error.html.twig',array(
+        		'titre'=> "Erreur Login",
+        		'message'=> "Erreur"
+        	));
+         	}
+
+
+
+         }
+
         return $this->render('RollRollBundle:User:login.html.twig',array(
         	'Login'=> $form->createView()
         ));
+
+
     }
 }

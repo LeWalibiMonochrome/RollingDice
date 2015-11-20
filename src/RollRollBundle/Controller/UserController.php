a1a4c1757f3e06af91590d93188467c66a1ba5a1
Nicolas Gauville <noreo.ercoli@gmail.com>
2015-11-20 15:42:29 +0100

    tests connexion

--- src/RollRollBundle/Controller/UserController.php
+++ src/RollRollBundle/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace RollRollBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use RollRollBundle\Form\LoginType;
 use RollRollBundle\Entity\Player;
 
@@ -13,7 +14,7 @@ class UserController extends Controller
     /**
      * @Route("/login")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
 
     	$player = new Player();
@@ -22,7 +23,7 @@ class UserController extends Controller
             'method' => 'POST',
         ));
 
-         if ($form->handleRequest($request)->isValid()){
+        if ($form->handleRequest($request)->isValid()){
 
          	$repo = $this->getDoctrine()->getRepository("RollRollBundle:Player");
          	$user = $repo->findOneByPseudo($player->getPseudo());
@@ -34,21 +35,46 @@ class UserController extends Controller
 
 
 
-         	}else{
-         	return $this->render('RollRollBundle:Default:error.html.twig',array(
-        		'titre'=> "Erreur Login",
-        		'message'=> "Erreur"
-        	));
+         	} else {
+             	return $this->render('RollRollBundle:Default:error.html.twig',array(
+            		'titre'=> "Erreur Login",
+            		'message'=> "Erreur"
+            	));
          	}
-
-
-
-         }
+        }
 
         return $this->render('RollRollBundle:User:login.html.twig',array(
         	'Login'=> $form->createView()
         ));
+    }
 
+    /**
+     * @Route("/liste")
+     */
+    public function listeUsersAction()
+    {
+        $repo = $this->getDoctrine()->getRepository("RollRollBundle:Player");
 
+        return $this->render('RollRollBundle:Test:users.html.twig',array(
+            'users' => $repo->findAll()
+        ));
+    }
+
+    /**
+     * @Route("/creerUser")
+     */
+    public function createUserAction()
+    {
+        $user = new Player();
+        $user->setPseudo('Bob');
+        $user->setPassword('bob');
+
+        $this->getDoctrine()->getManager()->persist($user);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->render('RollRollBundle:Default:error.html.twig',array(
+            'titre'=> "User créé",
+            'message'=> "ID : " . $user->getId()
+        ));
     }
 }

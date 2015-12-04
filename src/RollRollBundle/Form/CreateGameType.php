d657a755eddc3208cb4829712c46fb194de12b9e
LeWalibiMonochrome <novano75@outlook.fr>
2015-12-04 11:47:07 +0100

    creation parti

---
+++ src/RollRollBundle/Form/CreateGameType.php
@@ -0,0 +1,21 @@
+<?php
+// src/RollRollBundle/Form/LoginType.php
+
+namespace RollRollBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LoginType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('nbPlayers',      'number')
+      ->add('name',     'text')
+      ->add('create',      'submit')
+    ;
+  }
+
+}

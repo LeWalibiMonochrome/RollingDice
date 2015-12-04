cbc43004af43bc8c643e1b63700c6a0fefdadcd7
Nicolas Gauville <noreo.ercoli@gmail.com>
2015-12-04 11:48:15 +0100

    création de partie

--- src/RollRollBundle/Controller/GameController.php
+++ src/RollRollBundle/Controller/GameController.php
@@ -5,6 +5,9 @@ namespace RollRollBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use RollRollBundle\Entity\Game;
+use RollRollBundle\Form\CreateGameType;
+
 class GameController extends UserAwareController
 {
     /**
@@ -22,7 +25,37 @@ class GameController extends UserAwareController
     	}
 
         return parent::renderPage('RollRollBundle:User:games.html.twig',array(
-        	'games' => $this->getDoctrine()->getRepository('RollRollBundle:Game')->findByOwner($user)
+        	'games' => $this->getDoctrine()->getRepository('RollRollBundle:Grid')->findByOwner($user)
+        ));
+    }
+
+    /**
+     * @Route("/createGame", name="createGame")
+     */
+    public function createGameAction(Request $request)
+    {
+
+        $game = new Game();
+        $game->setStatus(0);
+        $game->setPlayerOrder(' ');
+
+        $form = $this->createForm(new CreateGameType(), $game, array(
+            'action' => '',
+            'method' => 'POST',
+        ));
+
+        if ($form->handleRequest($request)->isValid()){
+            $this->getDoctrine()->getManager()->persist($game);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->render('RollRollBundle:Default:error.html.twig',array(
+                'titre'=> "Partie créée",
+                'message'=> "Partie ".$game->getName()." créée"
+            ));
+        }
+
+        return parent::renderPage('RollRollBundle:User:createGame.html.twig',array(
+            'form'=> $form->createView()
         ));
     }
 }

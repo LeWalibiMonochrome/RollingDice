0dea2665da70544f4a07078c2e9c544803e805c6
NGauville <noreo.ercoli@gmail.com>
2015-12-05 21:56:23 +0100

    requete XHR

---
+++ src/RollRollBundle/Controller/XHRGameController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RollRollBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use RollRollBundle\Entity\Game;
+use RollRollBundle\Entity\Grid;
+use RollRollBundle\Form\CreateGameType;
+
+class XHRGameController extends UserAwareController
+{
+    /**
+     * @Route("/xhr/gendices/{a}/{b}/{c}", name="xhr_gendices", requirements={"a":"y|n","b":"y|n","c":"y|n"})
+     */
+    public function gendicesAction()
+    {
+    	return new Response(rand(1,6).'/'.rand(1,6).'/'.rand(1,6));
+    }
+}
--- web/js/dice.js
+++ web/js/dice.js
@@ -26,9 +26,6 @@ var rollroll = rollroll || {};
 		  this.selected = !this.selected;
 		  this.updateSelection();
 	  }
-	  p.isSelected = function() {
-		  return this.selected;
-	  };
 	  p.updateSelection = function() {
 		  this.div.className = "dice" + (this.selected ? " diceSelected" : "");
 	  }
@@ -36,12 +33,17 @@ var rollroll = rollroll || {};
 		  document.getElementById('dice'+this.i+'_'+$i+'_'+$j).innerHTML = ($v?'<span class="diceBall"> </span>':'');
 	  };
 	  p.resetCases = function() {
-		  for(var i=0;i<3;i++) {
+		  	for(var i=0;i<3;i++) {
 			  for(var j=0;j<3;j++) {
 				  this.setCase(i,j,false);
 			  }
 		  }
 	  }
+	  p.setCases = function() {
+	  		for(var i=0,j=arguments.length;i<j;i+=2) {
+	  			this.setCase(arguments[i],arguments[i+1],true);
+	  		}
+	  };
 	  p.set = function($i) {
 		  if(!$i) {
 			  while(!$i || $i == this.val) {
@@ -51,42 +53,14 @@ var rollroll = rollroll || {};
 		  this.val = $i;
 		  this.resetCases();
 		  switch($i) {
-			  case 1:
-				  this.setCase(1,1,true);
-				  break;
-			  case 2:
-				  this.setCase(0,0,true);
-				  this.setCase(2,2,true);
-				  break;
-			  case 3:
-				  this.setCase(0,0,true);
-				  this.setCase(1,1,true);
-				  this.setCase(2,2,true);
-				  break;
-			  case 4:
-				  this.setCase(0,0,true);
-				  this.setCase(2,0,true);
-				  this.setCase(0,2,true);
-				  this.setCase(2,2,true);
-				  break;
-			  case 5:
-				  this.setCase(0,0,true);
-				  this.setCase(2,0,true);
-				  this.setCase(0,2,true);
-				  this.setCase(2,2,true);
-				  this.setCase(1,1,true);
-				  break;
-			  case 6:
-				  this.setCase(0,0,true);
-				  this.setCase(1,0,true);
-				  this.setCase(2,0,true);
-				  this.setCase(0,2,true);
-				  this.setCase(1,2,true);
-				  this.setCase(2,2,true);
-				  break;
+			  case 1: this.setCases(1,1); break;
+			  case 2: this.setCases(0,0,2,2); break;
+			  case 3: this.setCases(0,0,1,1,2,2); break;
+			  case 4: this.setCases(0,0,2,0,0,2,2,2); break;
+			  case 5: this.setCases(0,0,2,0,0,2,2,2,1,1); break;
+			  case 6: this.setCases(0,0,1,0,2,0,0,2,1,2,2,2); break;
 			  default: break;
 		  }
 	};
 	rollroll.Dice = Dice;
 })(rollroll);
-

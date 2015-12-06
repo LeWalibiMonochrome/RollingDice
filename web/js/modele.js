f0091339d9fc54905035c2febc66874b3baf6cdf
Mad-Otter <mad_otter@protonmail.ch>
2015-12-06 18:34:37 +0100

    Calcul du score et affichage de celui ci

--- web/js/modele.js
+++ web/js/modele.js
@@ -1,44 +1,48 @@
-var Score = {
-  colors : [0, 0, 0], // red, yellow, violet
-  pentagon : [0, 0, 0, 0, 0],
-  missed : 0,
-  total : 0
-};
-
 function Game(){
-  this.orange = [9,8,7,6,5,4,3,2,1];
+  this.orange = [9,8,0,0,5,4,0,2,1];
   this.jaune = [3,1,4,1,5,1,6,1,7];
   this.violet = [1,2,3,4,5,6,7,8,9];
+	this.missed = 0;
+	this.colorsScore = [0, 0, 0]; // orange, jaune, violet
+  this.pentagonScore = [0, 0, 0, 0, 0];
+  this.totalScore = 0;
 };
 
 var p = Game.prototype;
 
-/*p.calculateScore = function(){
-  colors[0] = 0; colors[1] = 0; colors[2] = 0;
+p.calculateScore = function(){
+  this.colorsScore[0] = 0; this.colorsScore[1] = 0; this.colorsScore[2] = 0;
+  for(var i=0; i<5; i++){
+		this.pentagonScore[i] = 0;
+	}
   for(var i=0; i<9; i++){
-    colors[0] = red[i]; colors[1] = yellow[i]; colors[2] = violet[i];
-  }
-  if (red[0] !== 0 && yellow[1] !== 0) pentagon[0] = violet[2];
-  if (yellow[2] !== 0 && violet[3] !== 0) pentagon[1] = red[1];
-  if (yellow[5] !== 0 && violet[6] !== 0) pentagon[2] = red[4];
-  if (red[5] !== 0 && violet[7] !== 0) pentagon[3] = yellow[6];
-  if (red[6] !== 0 && yellow[7] !== 0) pentagon[4] = violet[8];
-  total = colors[0] + colors[1] +  colors[2];
-  total += pentagon[0] + pentagon[1] + pentagon[2] + pentagon[3] + pentagon[4];
-  total -= missed * 5;
-};*/
+    this.colorsScore[0] = this.orange[i]; this.colorsScore[1] = this.jaune[i]; this.colorsScore[2] = this.violet[i];
+	}
+  if (this.orange[0] !== 0 && this.jaune[1] !== 0) this.pentagonScore[0] = this.violet[2];
+  if (this.jaune[2] !== 0 && this.violet[3] !== 0) this.pentagonScore[1] = this.orange[1];
+  if (this.jaune[5] !== 0 && this.violet[6] !== 0) this.pentagonScore[2] = this.orange[4];
+  if (this.orange[5] !== 0 && this.violet[7] !== 0) this.pentagonScore[3] = this.jaune[6];
+  if (this.orange[6] !== 0 && this.jaune[7] !== 0) this.pentagonScore[4] = this.violet[8];
+  
+	this.totalScore = this.colorsScore[0] + this.colorsScore[1] +  this.colorsScore[2];
+  this.totalScore += this.pentagonScore[0] + this.pentagonScore[1] + this.pentagonScore[2] + this.pentagonScore[3] + this.pentagonScore[4];
+  this.totalScore -= this.missed * 5;
+};
 
 p.getCase = function(color, position){
   switch(color){
-  case 0: 
+  case 0:
+		if (this.orange[position] === 0) 
+			return "&nbsp;"
     return this.orange[position];
-    break;
   case 1:
+		if (this.jaune[position] === 0) 
+			return "&nbsp;"
     return this.jaune[position];
-    break;
   case 2:
-    return this.violet[position];
-    break;
+    if (this.violet[position] === 0) 
+			return "&nbsp;"
+		return this.violet[position];
   }
 };
 
@@ -46,6 +50,6 @@ p.chooseCase = function(color, position){
   
 };
 
-p.chooseDices = function(red, yellow, violet){
+p.chooseDices = function(orange, jaune, violet){
   
 };

be34804a1990ae69731ffea76fb79feff5814617
Mad-Otter <mad_otter@protonmail.ch>
2015-12-10 14:39:27 +0100

    Mise a jour modele

--- web/js/dice.js
+++ web/js/dice.js
@@ -1,66 +1,66 @@
 var rollroll = rollroll || {};
 
 (function(rollroll){
-	var Dice = function($div,$i,$clr) {
-		  this.div = document.getElementById($div);
-		  this.i = $i;
-		  this.val = 0;
-		  this.selected = false;
-
-		  $clr = $clr || '#FFFFFF';
-		  this.div.style.background = $clr;
-
-		  var s = '<table>';
-		  for(var i=0; i<3; i++) {
-			  s += '<tr>';
-			  for(var j=0; j<3; j++) {
-				  s += '<td id="dice'+this.i+'_'+i+'_'+j+'"></td>';
-			  }
-			  s += '</tr>';
-		  }
-		  this.div.innerHTML = s+'</table>';
-		  this.updateSelection();
-	  };
-	  var p = Dice.prototype;
-	  p.select = function() {
-		  this.selected = !this.selected;
-		  this.updateSelection();
-	  }
-	  p.updateSelection = function() {
-		  this.div.className = "dice" + (this.selected ? " diceSelected" : "");
-	  }
-	  p.setCase = function($i,$j,$v) {
-		  document.getElementById('dice'+this.i+'_'+$i+'_'+$j).innerHTML = ($v?'<span class="diceBall"> </span>':'');
-	  };
-	  p.resetCases = function() {
-		  	for(var i=0;i<3;i++) {
-			  for(var j=0;j<3;j++) {
-				  this.setCase(i,j,false);
-			  }
-		  }
-	  }
-	  p.setCases = function() {
-	  		for(var i=0,j=arguments.length;i<j;i+=2) {
-	  			this.setCase(arguments[i],arguments[i+1],true);
-	  		}
-	  };
-	  p.set = function($i) {
-		  if(!$i) {
-			  while(!$i || $i == this.val) {
-				  $i = 1+Math.floor(Math.random()*6);
-			  }
-		  }
-		  this.val = $i;
-		  this.resetCases();
-		  switch($i) {
-			  case 1: this.setCases(1,1); break;
-			  case 2: this.setCases(0,0,2,2); break;
-			  case 3: this.setCases(0,0,1,1,2,2); break;
-			  case 4: this.setCases(0,0,2,0,0,2,2,2); break;
-			  case 5: this.setCases(0,0,2,0,0,2,2,2,1,1); break;
-			  case 6: this.setCases(0,0,1,0,2,0,0,2,1,2,2,2); break;
-			  default: break;
-		  }
-	};
-	rollroll.Dice = Dice;
+  var Dice = function($div,$i,$clr) {
+    this.div = document.getElementById($div);
+    this.i = $i;
+    this.val = 0;
+    this.selected = false;
+    
+    $clr = $clr || '#FFFFFF';
+    this.div.style.background = $clr;
+    
+    var s = '<table>';
+    for(var i=0; i<3; i++) {
+      s += '<tr>';
+      for(var j=0; j<3; j++) {
+	s += '<td id="dice'+this.i+'_'+i+'_'+j+'"></td>';
+      }
+      s += '</tr>';
+    }
+    this.div.innerHTML = s+'</table>';
+    this.updateSelection();
+  };
+  var p = Dice.prototype;
+  p.select = function() {
+    this.selected = !this.selected;
+    this.updateSelection();
+  }
+  p.updateSelection = function() {
+    this.div.className = "dice" + (this.selected ? " diceSelected" : "");
+  }
+  p.setCase = function($i,$j,$v) {
+    document.getElementById('dice'+this.i+'_'+$i+'_'+$j).innerHTML = ($v?'<span class="diceBall"> </span>':'');
+  };
+  p.resetCases = function() {
+    for(var i=0;i<3;i++) {
+      for(var j=0;j<3;j++) {
+	this.setCase(i,j,false);
+      }
+    }
+  }
+  p.setCases = function() {
+    for(var i=0,j=arguments.length;i<j;i+=2) {
+      this.setCase(arguments[i],arguments[i+1],true);
+    }
+  };
+  p.set = function($i) {
+    if(!$i) {
+      while(!$i || $i == this.val) {
+	$i = 1+Math.floor(Math.random()*6);
+      }
+    }
+    this.val = $i;
+    this.resetCases();
+    switch($i) {
+    case 1: this.setCases(1,1); break;
+    case 2: this.setCases(0,0,2,2); break;
+    case 3: this.setCases(0,0,1,1,2,2); break;
+    case 4: this.setCases(0,0,2,0,0,2,2,2); break;
+    case 5: this.setCases(0,0,2,0,0,2,2,2,1,1); break;
+    case 6: this.setCases(0,0,1,0,2,0,0,2,1,2,2,2); break;
+    default: break;
+    }
+  };
+  rollroll.Dice = Dice;
 })(rollroll);
--- web/js/modele.js
+++ web/js/modele.js
@@ -1,55 +1,51 @@
-function Game(){
-  this.orange = [9,8,0,0,5,4,0,2,1];
-  this.jaune = [3,1,4,1,5,1,6,1,7];
-  this.violet = [1,2,3,4,5,6,7,8,9];
-	this.missed = 0;
-	this.colorsScore = [0, 0, 0]; // orange, jaune, violet
-  this.pentagonScore = [0, 0, 0, 0, 0];
-  this.totalScore = 0;
-};
+var model = model || {};
 
-var p = Game.prototype;
-
-p.calculateScore = function(){
-  this.colorsScore[0] = 0; this.colorsScore[1] = 0; this.colorsScore[2] = 0;
-  for(var i=0; i<5; i++){
-		this.pentagonScore[i] = 0;
-	}
-  for(var i=0; i<9; i++){
-    this.colorsScore[0] = this.orange[i]; this.colorsScore[1] = this.jaune[i]; this.colorsScore[2] = this.violet[i];
-	}
-  if (this.orange[0] !== 0 && this.jaune[1] !== 0) this.pentagonScore[0] = this.violet[2];
-  if (this.jaune[2] !== 0 && this.violet[3] !== 0) this.pentagonScore[1] = this.orange[1];
-  if (this.jaune[5] !== 0 && this.violet[6] !== 0) this.pentagonScore[2] = this.orange[4];
-  if (this.orange[5] !== 0 && this.violet[7] !== 0) this.pentagonScore[3] = this.jaune[6];
-  if (this.orange[6] !== 0 && this.jaune[7] !== 0) this.pentagonScore[4] = this.violet[8];
+(function(model){
+  function Game(){
+    this.orange = [9,8,0,0,5,4,0,2,1];
+    this.jaune = [3,1,4,1,5,1,6,1,7];
+    this.violet = [1,2,3,4,5,6,7,8,9];
+    this.missed = 0;
+    this.colorsScore = [0, 0, 0]; // orange, jaune, violet
+    this.pentagonScore = [0, 0, 0, 0, 0];
+    this.totalScore = 0;
+  };
   
-	this.totalScore = this.colorsScore[0] + this.colorsScore[1] +  this.colorsScore[2];
-  this.totalScore += this.pentagonScore[0] + this.pentagonScore[1] + this.pentagonScore[2] + this.pentagonScore[3] + this.pentagonScore[4];
-  this.totalScore -= this.missed * 5;
-};
+  var p = Game.prototype;
 
-p.getCase = function(color, position){
+  p.calculateScore = function(){ //
+    this.colorsScore[0] = 0; this.colorsScore[1] = 0; this.colorsScore[2] = 0;
+    for(var i=0; i<5; i++){
+      this.pentagonScore[i] = 0;
+    }
+    for(var i=0; i<9; i++){
+      this.colorsScore[0] = this.orange[i]; this.colorsScore[1] = this.jaune[i]; this.colorsScore[2] = this.violet[i];
+    }
+    if (this.orange[0] !== 0 && this.jaune[1] !== 0) this.pentagonScore[0] = this.violet[2];
+    if (this.jaune[2] !== 0 && this.violet[3] !== 0) this.pentagonScore[1] = this.orange[1];
+    if (this.jaune[5] !== 0 && this.violet[6] !== 0) this.pentagonScore[2] = this.orange[4];
+    if (this.orange[5] !== 0 && this.violet[7] !== 0) this.pentagonScore[3] = this.jaune[6];
+    if (this.orange[6] !== 0 && this.jaune[7] !== 0) this.pentagonScore[4] = this.violet[8];
+    
+    this.totalScore = this.colorsScore[0] + this.colorsScore[1] +  this.colorsScore[2];
+    this.totalScore += this.pentagonScore[0] + this.pentagonScore[1] + this.pentagonScore[2] + this.pentagonScore[3] + this.pentagonScore[4];
+    this.totalScore -= this.missed * 5;
+};
+  
+  p.getCase = function(color, position){
   switch(color){
   case 0:
-		if (this.orange[position] === 0) 
-			return "&nbsp;"
+    if (this.orange[position] === 0) 
+      return "&nbsp;"
     return this.orange[position];
   case 1:
-		if (this.jaune[position] === 0) 
-			return "&nbsp;"
+    if (this.jaune[position] === 0) 
+      return "&nbsp;"
     return this.jaune[position];
   case 2:
     if (this.violet[position] === 0) 
-			return "&nbsp;"
-		return this.violet[position];
+      return "&nbsp;"
+    return this.violet[position];
   }
-};
-
-p.chooseCase = function(color, position){
-  
-};
-
-p.chooseDices = function(orange, jaune, violet){
-  
-};
+  };  
+})(model);
